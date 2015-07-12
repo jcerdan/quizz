4dfb7562a869de037a81af2abc1cacdd157c26b9
Jordi Cerdan <jcerdan@tecob.com>
2015-07-12 17:30:02 +0200

    Added author page and link to github

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,9 @@ var quizController = require("../controllers/quizz_controller.js");
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quizz' });
 });
+router.get('/author', function(req, res, next){
+	res.render('author');
+});
 
 router.get('/quizzes/question', quizController.question);
 router.get('/quizzes/answer', quizController.answer);
