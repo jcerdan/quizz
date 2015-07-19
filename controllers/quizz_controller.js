893d60885786938e4cf8613dbf83e0388fc498a4
Jordi Cerdan <jcerdan@tecob.com>
2015-07-19 20:45:44 +0200

    Added pg-hstore library

--- controllers/quizz_controller.js
+++ controllers/quizz_controller.js
@@ -1,17 +1,31 @@
 var models = require("../models/models.js");
 
-exports.question = function(req, res){
-	models.Quizz.findAll().then(function(quiz){
-		res.render('quizzes/question', {pregunta: quiz[0].pregunta});
+exports.show = function(req, res){
+	models.Quizz.find(req.params.quizzId).then(function(quizz){
+		res.render('quizzes/show', {
+			quizz: quizz,
+		});
 	});
 };
 
 exports.answer = function(req, res){
-	models.Quizz.findAll().then(function(quiz){
-		if (req.query.respuesta === quiz[0].respuesta)
-			res.render('quizzes/answer', {respuesta: 'Correcto'});
+	models.Quizz.find(req.params.quizzId).then(function(quizz){
+		if (req.query.respuesta === quizz.respuesta)
+			res.render('quizzes/answer', {
+				respuesta: 'Correcto'
+			});
 		else
-			res.render('quizzes/answer', {respuesta: 'Incorrecto'});
+			res.render('quizzes/answer', {
+				respuesta: 'Incorrecto'
+			});
 	});
+
+	exports.index = function(req, res){
+		models.Quizz.findAll().then(function(quizzes){
+			res.render('quizzes/index', {
+				quizzes: quizzes,
+			})
+		})
+	};
 	
 };
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-var quizController = require("../controllers/quizz_controller.js");
+var quizzController = require("../controllers/quizz_controller.js");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,7 +11,8 @@ router.get('/author', function(req, res, next){
 	res.render('author');
 });
 
-router.get('/quizzes/question', quizController.question);
-router.get('/quizzes/answer', quizController.answer);
+router.get('/quizzes', 												quizzController.index);
+router.get('/quizzes/:quizzId(\\d+)', 				quizzController.show);
+router.get('/quizzes/:quizzId(\\d+)/answer', 	quizzController.answer);
 
 module.exports = router;
