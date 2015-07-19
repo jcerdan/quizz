38772895b8304f350fa76841d7dfbd85746ca05f
Jordi Cerdan <jcerdan@tecob.com>
2015-07-19 20:06:12 +0200

    modificado sistema para trabajar con SQLite en local y PostgreSQL en Heroku

--- models/models.js
+++ models/models.js
@@ -1,9 +1,23 @@
 var path = require("path");
 var Sequelize = require("sequelize");
 
-var sequelize = new Sequelize(null, null, null, {
-  dialect: 'sqlite',
-  storage: path.join(__dirname, 'quizz.sqlite')
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name  = (url[6]||null);
+var user     = (url[2]||null);
+var pwd      = (url[3]||null);
+var protocol = (url[1]||null);
+var dialect  = (url[1]||null);
+var port     = (url[5]||null);
+var host     = (url[4]||null);
+var storage  = process.env.DATABASE_STORAGE;
+
+var sequelize = new Sequelize(DB_name, user, pwd, {
+  dialect: dialect,
+  protocol: protocol,
+  host: host,
+  port: port,
+  storage: storage,
+  omitNull: true
 });
 
 var Quizz = sequelize.import(path.join(__dirname, 'quizz'));
